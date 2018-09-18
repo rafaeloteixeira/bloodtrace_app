9a1d8bed8e73faaf50404e1187d4d56c96480a72
Rafael Oliveira Teixeira <rafael.ot@outlook.com>
2018-09-18 20:51:15 -0300

    Add project files.

---
+++ BloodTrace/BloodTrace/Models/BloodUser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodTrace.Models
+{
+    public class BloodUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string BloodGroup { get; set; }
+        public string ImagePath { get; set; }
+        public string FullLogoPath
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(ImagePath))
+                {
+                    return string.Empty;
+                }
+                return string.Format("http://bloodtraceapptest.azurewebsites.net/{0}", ImagePath.Substring(1));
+            }
+            set
+            {
+            }
+        }
+        public int Date { get; set; }
+        public object ImageArray { get; set; }
+    }
+}
---
+++ BloodTrace/BloodTrace/Pages/DonorsListPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using BloodTrace.Models;
+using BloodTrace.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BloodTrace.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DonorsListPage : ContentPage   
+	{
+        public ObservableCollection<BloodUser> BloodUsers;
+        private string _bloodGroup;
+        private string _city;
+        private string _state;
+
+        public DonorsListPage (string city, string state, string bloodType)
+		{
+			InitializeComponent ();
+            BloodUsers = new ObservableCollection<BloodUser>();
+            _bloodGroup = bloodType;
+            _city = city;
+            _state = state;
+            FindBloodDonors();
+		}
+
+        private async void FindBloodDonors()
+        {
+            ApiServices apiServices = new ApiServices();
+            var bloodUsers = await apiServices.FindBlood(_state, _city, _bloodGroup);
+            foreach (var bloodUser in bloodUsers)
+            {
+                BloodUsers.Add(bloodUser);
+            }
+
+            LvBloodDonors.ItemsSource = BloodUsers;
+        }
+
+        private void LvBloodDonors_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var selectedDonor = e.SelectedItem as BloodUser;
+            Navigation.PushAsync(new DonorProfilePage(selectedDonor));
+        }
+    }
+}
